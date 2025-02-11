b618d96fbfe713a228edbf1d005abbb7aac8e8f8
anyhony58310 <anthony.regala58@gmail.com>
2025-02-11 15:46:15 +0100

    eslintBuildProd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,10 @@ export default [
       'no-alert': 'error', // Ajout de la règle pour interdire alert()
       'no-ternary': 'warn', // Ajout de la règle pour déclencher un avertissement sur l'opérateur ternaire
       'no-console': ['warn', { allow: ['log'] }], // Avertissement pour console.log(), mais ignoré
+      'no-warning-comments': [
+        isBuilding ? 'error' : 'off', // Active la règle en mode production (build)
+        { terms: ['todo', 'fixme'], location: 'anywhere' }, // Déclenche une erreur sur les commentaires 'todo' et 'fixme'
+      ],
     },
   },
 ];
