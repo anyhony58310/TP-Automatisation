ff56acbbec16b53c3335bf687e137c9fb98a4251
anyhony58310 <anthony.regala58@gmail.com>
2025-02-11 09:23:06 +0100

    prettierConfig

--- coucou.js
+++ coucou.js
@@ -1,5 +1,15 @@
 function coucou(){
     console.log("Coucou, c'est moi, le changement !");
 }
-
 coucou();
+  
+function coucouPerso(name){
+    console.log(`Coucou, ${name}!`);
+}
+coucouPerso('REGALA Anthony');
+
+function coucouQuote(){
+    console.log("Coucou, je préfère les quotes simples !");
+}
+coucouQuote();
+  
